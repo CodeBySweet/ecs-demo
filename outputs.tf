f7cbb017149f12c01e0b4bff418f8ae27d80cff7
CodeBySweet <sh.ziyovuddinova@gmail.com>
2025-03-27 16:30:21 -0500

    updates

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,9 @@ output "ecs_cluster_name" {
   value = aws_ecs_cluster.my_cluster.name
 }
 
-# output "ecr_repository_url" {
-#   value = aws_ecr_repository.my_repo.repository_url
-# }
+output "ecr_repository_url" {
+  value = aws_ecr_repository.my_repo.repository_url
+}
 
 output "ecs_service_name" {
   value = aws_ecs_service.my_service.name
