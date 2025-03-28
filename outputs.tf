88129f0f4d2cf55794b020ef646cf88bc91b0bae
CodeBySweet <sh.ziyovuddinova@gmail.com>
2025-03-27 23:57:44 -0500

    updates

--- outputs.tf
+++ outputs.tf
@@ -26,7 +26,3 @@ output "ecr_repository_name" {
 output "ecs_task_execution_role_arn" {
   value = aws_iam_role.ecs_task_execution_role.arn
 }
-
-# output "ecs_task_role_arn" {
-#   value = aws_iam_role.ecs_task_role.arn
-# }
