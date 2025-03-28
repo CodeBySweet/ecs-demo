60c918ee5363695eb5db218959a37bfac9676cd4
CodeBySweet <sh.ziyovuddinova@gmail.com>
2025-03-27 23:49:08 -0500

    updates

--- outputs.tf
+++ outputs.tf
@@ -21,3 +21,12 @@ output "ecr_repository_name" {
 # output "task_definition_content" {
 #   value = aws_ecs_task_definition.my_task.container_definitions
 # }
+
+
+output "ecs_task_execution_role_arn" {
+  value = aws_iam_role.ecs_task_execution_role.arn
+}
+
+output "ecs_task_role_arn" {
+  value = aws_iam_role.ecs_task_role.arn
+}
