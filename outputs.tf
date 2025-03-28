cdbc81bcbfe47e66f34897d74843b95d00dff337
CodeBySweet <sh.ziyovuddinova@gmail.com>
2025-03-27 19:12:54 -0500

    updates

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "ecr_repository_url" {
 output "ecs_service_name" {
   value = aws_ecs_service.my_service.name
 }
+
+output "ecr_repository_name" {
+  value = aws_ecr_repository.my_repo.name
+}
